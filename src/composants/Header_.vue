4c5d5bab1200b8ddf47e5c1b851cdc9d726bf53a
BARRY007 <alphabarryfr224@gmail.com>
2025-03-23 19:38:47 +0000

    a

--- src/composants/Header_.vue
+++ src/composants/Header_.vue
@@ -3,13 +3,13 @@
     <div class="bg-blue-800 p-1 mb-2 shadow-2xl flex w-full justify-between">
         <h1 class="text-white">Word_metéo</h1>
         <div class="flex justify-start align-center mb-1.5 gap-10 pr-10 max-md:hidden">
-        <a href="#" class="text-red-300 font-semibold" @click="envoyer(1)">Accueil</a>
-        <a href="#" class="text-gray-300 font-semibold" @click="envoyer(0)">Météo du monde </a>
+        <a href="#" class="text-red-300 font-semibold" @click="envoyer(1,0)">Accueil</a>
+        <a href="#" class="text-gray-300 font-semibold" @click="envoyer(0,0)">Météo du monde </a>
         </div>
         <div class=" md:hidden"><i class="fas fa-bars text-xl text-white mr-2" @click="swit"></i></div>
         <div class="absolute top-8 bg-blue-400 flex flex-col gap-2 m-2 right-1.5 p-2 hidden" id="swit">
-        <a href="#" class="text-white font-semibold" @click="envoyer(1)">Accueil</a>
-        <a href="#" class="text-white font-semibold" @click="envoyer(0)">Météo du monde </a>
+        <a href="#" class="text-white font-semibold" @click="envoyer(1,1)">Accueil</a>
+        <a href="#" class="text-white font-semibold" @click="envoyer(0,1)">Météo du monde </a>
       </div> 
     </div>
   <form @submit.prevent="clique">
@@ -42,7 +42,10 @@ const ville= ref('')
      ville.value=''
  }
 
-function envoyer(params) {
+function envoyer(params,pars2) {
     emits("menu",params)
+    
+    if(pars2==1)
+    swit()
 }
 </script>
